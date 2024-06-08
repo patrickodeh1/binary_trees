d10e78248f51923a8bc78f6c2c46a0ff1848bb06
Patrick Odeh <vyltyan@gmail.com>
2024-06-08 11:44:20 +0100

    3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - inserts a node as a right child
+ * @tree: tree to delete
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (NULL);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
+}
