27c0ccddc1af33d27e51dbfc69aa9f1a0e21515f
Patrick Odeh <vyltyan@gmail.com>
2024-06-08 11:59:28 +0100

    3-binary_tree_delete.c

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,7 @@
 #define BINARY_TREES
 
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -17,7 +18,7 @@ struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
 
 typedef struct binary_tree_s binary_tree_t;
 
